f0711903eaac166fa2e5b9ff4f5d277386265759
PC <PC@DESKTOP-060UP96>
2026-07-05 21:41:24 +0200

    Initial commit - postavljen Maven i struktura

---
+++ src/main/java/rs/fon/fudbalski/klub/FudbalskiKlub.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package rs.fon.fudbalski.klub;
+
+/**
+ *
+ * @author PC
+ */
+public class FudbalskiKlub {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
